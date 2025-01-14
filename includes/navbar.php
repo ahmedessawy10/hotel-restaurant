9e99faae1af1c8a1c114f3b762276d4ff9a4dfe7
SalmaOsama1612 <salmaosamaas287@gmail.com>
2025-01-14 18:30:58 +0200

    commiting

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-lg nav-color ">
+<nav class="navbar navbar-expand-lg nav-color mb-5" style="background-color: #e8d59e;">
     <div class="container-fluid">
-        <a class="navbar-brand " href="#">
-            <img src="<?php echo $baseURL; ?>/assets/images/logo.png" class="w-49" alt="logo">
+        <a class="navbar-brand" href="#">
+            <img class="w-50 rounded-circle" src="<?php echo $baseURL; ?>/assets/images/logo.png" alt="logo">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,24 +10,23 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link " aria-current="page" href="#">Home</a>
+                    <a class="nav-link" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active " aria-current="page" href="<?php echo $baseURL; ?>/pages/admin/products.html">products</a>
+                    <a class="nav-link active" aria-current="page" href="<?php echo $baseURL; ?>/pages/admin/products.html">Products</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " aria-current="page" href="<?php echo $baseURL; ?>/pages/admin/users.html">users</a>
+                    <a class="nav-link" aria-current="page" href="<?php echo $baseURL; ?>/pages/admin/users.html">Users</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " aria-current="page" href="#">manual order</a>
+                    <a class="nav-link" aria-current="page" href="#">Manual Order</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " aria-current="page" href="#">check</a>
+                    <a class="nav-link" aria-current="page" href="#">Check</a>
                 </li>
             </ul>
-            <span class=" me-0 border-1 rounded-circle txt-color"><i class="fa-solid fa-user "></i></span>
-            <p class="txt-color mt-2 pt-2 ps-2 ">admin name</p>
-
+            <span class="me-0 border-1 rounded-circle txt-color"><i class="fa-solid fa-user"></i></span>
+            <p class="txt-color mt-2 pt-2 ps-2">Admin Name</p>
         </div>
     </div>
 </nav>
--- pages/profile/myprofile.php
+++ pages/profile/myprofile.php
@@ -10,166 +10,131 @@ require_once "../../includes/navbar.php";
 ?>
 
 
-<div class="myprofile">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Profile</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="myprofile">
+        <div class="container">
+            <div class="row align-items-start">
+                <!-- Vertical Navigation -->
+                <div class="col-md-3 nav d-flex flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">My Profile</button>
+                    <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Password</button>
+                </div>
 
-    <div class="container ">
-
-        <div class="row align-items-start ">
-            <div class="col-md-3 nav d-flex flex-column nav-pills " id="v-pills-tab" role="tablist"
-                aria-orientation="vertical">
-                <button class="nav-link active " id="v-pills-home-tab" data-bs-toggle="pill"
-                    data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home"
-                    aria-selected="true">my profile</button>
-                <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill"
-                    data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile"
-                    aria-selected="false">password</button>
-
-
-            </div>
-            <div class=" col-md-9 tab-content" id="v-pills-tabContent">
-
-                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel"
-                    aria-labelledby="v-pills-home-tab" tabindex="0">
-                    <h2 class=" p-2 w-100"> my profile</h2>
-
-                    <div class="prfile_image d-flex justify-content-center center align-items-center  my-3">
-                        <button class="btn " data-toggle="modal" type="button" data-target="#updateImage">
-                            <img src=" ../../assets/images/logo.png" alt="">
-                        </button>
+                <!-- Tab Content -->
+                <div class="col-md-9 tab-content" id="v-pills-tabContent">
+                    <!-- My Profile Tab -->
+                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
+                        <h2 class="p-2 w-100">My Profile</h2>
+                        <div class="prfile_image d-flex justify-content-center align-items-center my-3">
+                            <button class="btn" data-toggle="modal" type="button" data-target="#updateImage">
+                                <img src="../../assets/images/logo.png" alt="Profile Image">
+                            </button>
+                        </div>
+                        <form action="">
+                            <div class="row justify-content-center">
+                                <div class="col-md-10 my-2">
+                                    <div class="form-group">
+                                        <label for="name">Name</label>
+                                        <input type="text" class="form-control" id="name" placeholder="Enter name">
+                                    </div>
+                                </div>
+                                <div class="col-md-10 my-2">
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input type="email" class="form-control" id="email" placeholder="Enter Email">
+                                    </div>
+                                </div>
+                                <div class="col-md-10 my-2">
+                                    <div class="form-group">
+                                        <label for="phone">Phone</label>
+                                        <input type="text" class="form-control" id="phone" placeholder="Enter phone">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-center py-3">
+                                <button type="submit" class="btn btn-primary">Update</button>
+                            </div>
+                        </form>
                     </div>
 
-
-                    <form action="">
-
-                        <div class="row justify-content-center">
-                            <div class="col-md-10 my-2">
-                                <div class="form-group">
-                                    <label for="name">Name</label>
-                                    <input type="text" class="form-control" id="name" aria-describedby="emailHelp"
-                                        placeholder="Enter name">
+                    <!-- Change Password Tab -->
+                    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">
+                        <h2 class="p-2 w-100">Change Password</h2>
+                        <form action="">
+                            <div class="row justify-content-center">
+                                <div class="col-md-10 my-2">
+                                    <div class="form-group">
+                                        <label for="oldPassword">Old Password</label>
+                                        <input type="password" class="form-control" id="oldPassword" placeholder="Enter old password">
+                                    </div>
+                                </div>
+                                <div class="col-md-10 my-2">
+                                    <div class="form-group">
+                                        <label for="newPassword">New Password</label>
+                                        <input type="password" class="form-control" id="newPassword" placeholder="Enter new password">
+                                    </div>
+                                </div>
+                                <div class="col-md-10 my-2">
+                                    <div class="form-group">
+                                        <label for="confirmPassword">Confirm Password</label>
+                                        <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm new password">
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-md-10 my-2 ">
-                                <div class="form-group">
-                                    <label for="email">Email</label>
-                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
-                                        placeholder="Enter Email">
-                                </div>
-
+                            <div class="d-flex justify-content-center py-3">
+                                <button type="submit" class="btn btn-primary">Update</button>
                             </div>
-                            <div class="col-md-10 my-2 ">
-                                <div class="form-group">
-                                    <label for="phone">Phone</label>
-                                    <input type="text" class="form-control" id="phone" aria-describedby="phoneHelp"
-                                        placeholder="Enter phone">
-                                </div>
-
-                            </div>
-
-                        </div>
-
-
-                        <div class="d-flex justify-content-center py-3">
-                            <button type="submit" class="btn btn-primary">update</button>
-                        </div>
-                    </form>
-
+                        </form>
+                    </div>
                 </div>
-                <!-- change password -->
-                <div class=" tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab"
-                    tabindex="0">
-                    <h2 class=" p-2 w-100 ">change password</h2>
-
-                    <form action=" ">
-
-                        <div class="row justify-content-center">
-                            <div class="col-md-10 my-2">
-                                <div class="form-group">
-                                    <label for="oldPassword">old password</label>
-                                    <input type="text" class="form-control" id="oldPassword"
-                                        aria-describedby="emailHelp" placeholder="Enter oldPassword">
-                                </div>
-                            </div>
-                            <div class="col-md-10 my-2 ">
-                                <div class="form-group">
-                                    <label for="email">Email</label>
-                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
-                                        placeholder="Enter Email">
-                                </div>
-
-                            </div>
-                            <div class="col-md-10 my-2 ">
-                                <div class="form-group">
-                                    <label for="phone">Phone</label>
-                                    <input type="text" class="form-control" id="phone" aria-describedby="phoneHelp"
-                                        placeholder="Enter phone">
-                                </div>
-
-                            </div>
-
-                        </div>
-
-
-                        <div class="d-flex justify-content-center py-3">
-                            <button type="submit" class="btn btn-primary">update</button>
-                        </div>
-
-                    </form>
-
-
-
-
-                </div>
-
-
             </div>
         </div>
-
     </div>
-</div>
 
-
-
-
-
-<!-- update image modal -->
-
-
-
-<div class=" modal fade" id="updateImage" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">update image</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div>
-                    <form action="" method="" menctype=" multipart/form-data">
-                        <div class="form-group
-                        ">
-                            <label for="image"><img src="" alt=""></label>
+    <!-- Update Image Modal -->
+    <div class="modal fade" id="updateImage" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Update Image</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <label for="image">Upload Image</label>
                             <input type="file" name="image" class="form-control-file" id="image">
                         </div>
-
-                        <div class="form-group
-                        ">
-
-                            <button type="submit" class="form-control-file">update</button>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </div>
                     </form>
                 </div>
-
-            </div>
-            <div class=" modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">change</button>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
 
 <?php include "../../includes/footer.php";  ?>
